4ae739fd7df46bc590f1fdfc6ce4def704b3428a
Jason Chung <jachu@netcompany.com>
2019-04-04 17:46:55 +0100

    Fixing errors form merge

--- EITC route planning/Models/TransportationType.cs
+++ EITC route planning/Models/TransportationType.cs
@@ -6,11 +6,18 @@ using System.Web;
 namespace EITC_route_planning.Models
 {
     public class TransportationType
+
+
     {
-        public TransportationType(string Type)
+        public TransportationType(string type, float speed, float weightLimit)
+        {
+            Type = type;
+            Speed = speed;
+            WeightLimit = weightLimit;
+        }
         public string Type { get; set; }
 
-        public int Speed { get; set; }
+        public float Speed { get; set; }
 
         public float WeightLimit { get; set; }
     }
