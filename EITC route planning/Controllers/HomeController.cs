be8b64753c4dcb0cabf00aafd033bc5d764b7881
nc-mahi <mahi@netcompany.com>
2019-04-04 14:49:14 +0200

    Added a categories in the controller

--- EITC route planning/Controllers/HomeController.cs
+++ EITC route planning/Controllers/HomeController.cs
@@ -3,11 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EITC_route_planning.Models;
 
 namespace EITC_route_planning.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult homePageModel()
+        {
+            var categories = GetAllCategories();
+            var model = new Shippment();
+            model.Categories = GetSelectedCategories(categories);
+
+            return View(model);
+        }
+        private IEnumerable<string> GetAllCategories()
+        {
+            return new List<string>
+            {
+                "Heavy",
+                "Not Heavy",
+                "Dangerous",
+                "Non Dangerous"
+            };
+        }
+
+        private IEnumerable<SelectListItem> GetSelectedCategories(IEnumerable<string> elements)
+        {
+            var selectList = new List<SelectListItem>();
+
+            // This will result in MVC rendering each item as:
+            //     <option value="Category Name">Category Name</option>
+            foreach (var element in elements)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element,
+                    Text = element
+                });
+            }
+
+            return selectList;
+        }
+
+        public void createShippment()
+        {
+            if (HttpContext.Request.RequestType == "POST")
+            {
+                var weight = Request.Form["id"];
+                var packageType = Request.Form["type"];
+                var fromCity = Request.Form["from"];
+                var toCity = Request.Form["to"];
+            }
+
+        }
+
+        public void verifyInputs()
+        {
+
+        }
+
+        public void searchRoute()
+        {
+
+        }
+
+
+
         public ActionResult Index()
         {
             return View();
---
+++ EITC route planning/Models/Shippment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.WebPages.Html;
+
+namespace EITC_route_planning.Models
+{
+    public class Shippment
+    {
+        [Required]
+        [Display(Name = "Weight")]
+        public int Weight { get; set; }
+
+        [Required]
+        [Display(Name = "Type of package")]
+        public string Category { get; set; }
+        
+        public IEnumerable<SelectListItem> Categories { get; set; }
+    }
+}
