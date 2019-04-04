9ee8a6faf9f1f37995c643ff70549b37eecdb532
mhth <mhth@netcompany.com>
2019-04-04 07:57:00 +0200

    initial setup

---
+++ EITC route planning/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EITC_route_planning
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
